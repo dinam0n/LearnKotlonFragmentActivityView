87f40610402b2592908117e5e69ba510e7e4e276
pds <d.pehterev@orn.su>
2024-10-03 15:53:21 +0700

    Initial commit

---
+++ app/src/main/java/ru/myproject/learnkotlonfragmentactivityview/SecondFragment.kt
@@ -0,0 +1,49 @@
+package ru.myproject.learnkotlonfragmentactivityview
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import ru.myproject.learnkotlonfragmentactivityview.databinding.FragmentSecondBinding
+import kotlin.coroutines.CoroutineContext
+
+class SecondFragment:Fragment(R.layout.fragment_second) {
+    private var binding : FragmentSecondBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding=FragmentSecondBinding.bind(view)
+        binding?.content?.text = arguments?.getString(ARG_DATA)
+        binding?.confirmButton?.setOnClickListener{closeWithResult("Confirm")}
+        binding?.rejectButton?.setOnClickListener{closeWithResult("Reject")}
+    }
+
+    private fun closeWithResult(data:String){
+        val resultRequestKey = arguments?.getString(RESULT_REQUEST_KEY).orEmpty()
+        val resultDataKey = arguments?.getString(RESULT_DATA_KEY)
+        val result = Bundle().apply { putString(resultDataKey,data)}
+        parentFragmentManager.setFragmentResult(resultRequestKey, result)
+        parentFragmentManager.popBackStack()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding=null
+    }
+
+    companion object{
+        private const val ARG_DATA = "arg_data"
+        private const val RESULT_REQUEST_KEY = "result_request_key"
+        private const val RESULT_DATA_KEY = "result_data_key"
+
+        fun newInstance(data: String, resultRequestKey:String, resultDataKey: String):SecondFragment{
+            return SecondFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_DATA, data)
+                    putString(RESULT_REQUEST_KEY,resultRequestKey)
+                    putString(RESULT_DATA_KEY,resultDataKey)
+                }
+            }
+        }
+    }
+
+}
